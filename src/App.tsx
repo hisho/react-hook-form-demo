c3a0fe1563c7eb54ea95ca9d5d66ecaf13c491e6
hisho <hisho.web@gmail.com>
2022-05-30 03:52:38 +0900

    📦 add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-export const App = () => {
-  return <></>
+import { ChakraProvider } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+type AppProps = {
+  children: ReactNode
+}
+
+export const App = ({ children }: AppProps) => {
+  return <ChakraProvider>{children}</ChakraProvider>
 }
